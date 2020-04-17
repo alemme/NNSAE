1aa6343760b7f4b5ea458192acffd3167b0eabc2
Andre Lemme <alemme01@t-online.de>
2020-04-17 14:26:54 +0200

    develop pytorch branch

--- Python/NNSAE.py
+++ Python/NNSAE.py
@@ -1,13 +1,13 @@
-## Class Definion of the Non-Negative Sparse AutoEncoder (NNSAE)
+# Class Definion of the Non-Negative Sparse AutoEncoder (NNSAE)
 # The class defines fields that store model parameters and implements methods of the
-# NNSAE. The NNSAE uses shared weights. The class is designed to be used with 
+# NNSAE. The NNSAE uses shared weights. The class is designed to be used with
 # non-negative data distributions.
 #
 # HowTo use this class:
 # - Preparations:
 #   1) create an NNSAE object by calling the constructor with your
 #   specifications and call the method init
-#   2) after loading the dataset, train the NNSAE by calling the method 
+#   2) after loading the dataset, train the NNSAE by calling the method
 #   train with your dataset.
 #   NOTE: if you want to apply multiple training epochs call this function repeatedly
 # - Apply trained NNSAE to new data:
@@ -25,9 +25,9 @@
 # of the authors. Please acknowledge the authors in any academic publications
 # that have made use of this code or part of it. Please use this BibTex for
 # reference:
-# 
-#    A. Lemme, R. F. Reinhart and J. J. Steil. 
-#    "Online learning and generalization of parts-based image representations 
+#
+#    A. Lemme, R. F. Reinhart and J. J. Steil.
+#    "Online learning and generalization of parts-based image representations
 #     by Non-Negative Sparse Autoencoders". Submitted to Neural Networks,
 #                              OR
 #    A. Lemme, R. F. Reinhart and J. J. Steil. "Efficient online learning of
@@ -43,118 +43,121 @@ import numpy as np
 # Make all matlib functions accessible at the top level via M.func()
 import numpy.matlib as M
 # Make some matlib functions accessible directly at the top level via, e.g. rand(3,3)
-from numpy.matlib import rand,zeros,ones
+from numpy.matlib import rand, zeros, ones
+
 
 class NNSAE:
 
-        ## Constructor for a NNSAE class 
-        # input: 
+        # Constructor for a NNSAE class
+        # input:
         #  - inpDim gives the Data sample dimension
         #  - hidDim specifies size of the hidden layer
         # output
         #  - net is the created Non-Negative Sparse Autoencoder
-    
+
     def __init__(self, inpDim, hidDim):
-        self.inpDim = inpDim #number of input neurons (and output neurons)
-        self.hidDim = hidDim #number of hidden neurons
-        
-        self.inp = zeros((self.inpDim,1))   #vector holding current input
-        self.out = zeros((self.hidDim,1))   #output neurons
-        self.g = zeros((self.hidDim,1))     #neural activity before non-linearity
-        self.h = zeros((self.hidDim,1))     #hidden neuron activation
-        self.a = ones((self.hidDim,1))     #slopes of activation functions
-        self.b = -3*ones((self.hidDim,1))     #biases of activation functions
+        self.inpDim = inpDim  # number of input neurons (and output neurons)
+        self.hidDim = hidDim  # number of hidden neurons
+
+        self.inp = zeros((self.inpDim, 1))  # vector holding current input
+        self.out = zeros((self.hidDim, 1))  # output neurons
+        # neural activity before non-linearity
+        self.g = zeros((self.hidDim, 1))
+        self.h = zeros((self.hidDim, 1))  # hidden neuron activation
+        self.a = ones((self.hidDim, 1))  # slopes of activation functions
+        self.b = -3*ones((self.hidDim, 1))  # biases of activation functions
         scale = 0.025
-        
-        self.W = scale * (2 * rand((self.inpDim, self.hidDim)) - 0.5 * ones((self.inpDim, self.hidDim))) + scale     #shared network weights, i.e. used to compute hidden layer activations and estimated outputs
-        print self.W.shape
-        
-        self.lrateRO = 0.01; #learning rate for synaptic plasticity of read-out layer (RO)
-        self.regRO = 0.0002; #numerical regularization constant
-        self.decayP = 0;     #decay factor for positive weights [0..1]
-        self.decayN = 1;     #decay factor for negative weights [0..1]
 
-        self.lrateIP = 0.001;    #learning rate for intrinsic plasticity (IP)
-        self.meanIP = 0.2;       #desired mean activity, a parameter of IP
+        # shared network weights, i.e. used to compute hidden layer activations and estimated outputs
+        self.W = scale * (2 * rand((self.inpDim, self.hidDim)) -
+                          0.5 * ones((self.inpDim, self.hidDim))) + scale
+        print(self.W.shape)
 
-        
-      
-        ## Apply new data
+        # learning rate for synaptic plasticity of read-out layer (RO)
+        self.lrateRO = 0.01
+        self.regRO = 0.0002  # numerical regularization constant
+        self.decayP = 0  # decay factor for positive weights [0..1]
+        self.decayN = 1  # decay factor for negative weights [0..1]
+
+        self.lrateIP = 0.001  # learning rate for intrinsic plasticity (IP)
+        self.meanIP = 0.2  # desired mean activity, a parameter of IP
+
+    def apply(self, X):
+        # Apply new data
         # Xhat = apply(net, X)
         # this function processes data sampels without learning
         #
-        # input: 
+        # input:
         #  - net is a Non-Negative Sparse Autoencoder object
         #  - X is a N x M matrix holding the data input, N is the number of samples and M the dimension of each data sample
         # output
         #  - Xhat reconstructed Input sampels N x M
-    def apply(self, X):
-	   #for entire data matrix
-	   Xhat = zeros(shape(X))
-	   for i in range(1,shape(X,1)):
-	   	self.inp = X[i,:].T
-	   	self.update()
-	   	Xhat[i,:] = self.out.T
-	   return Xhat    
-        
-         
-    ## Train the network
+        # for entire data matrix
+        Xhat = zeros(X.shape)
+        for i in range(1, X.shape[1]):
+            self.inp = X[i, :].T
+            self.update()
+            Xhat[i, :] = self.out.T
+        return Xhat
+
+    # Train the network
     # This function adapts the weight matrix W and parameters a and b
     # of the non-linear activation function (intrinsic plasticity)
     #
-    # input: 
+    # input:
     #  - X is a N x M matrix holding the data input, N is the number of samples and M the dimension of each data sample
+
     def train(self, X):
         numSamples = X.shape[0]
-        p = np.random.permutation(range(numSamples)) #randperm(numSamples); #for randomized presentation
-        for i in range(1,numSamples):
-            #set input
-            self.inp = X[p[i],:].T            #forward propagation of activities
+        # randperm(numSamples); #for randomized presentation
+        p = np.random.permutation(range(numSamples))
+        for i in range(1, numSamples):
+            # set input
+            self.inp = X[p[i], :].T  # forward propagation of activities
             self.update()
-            
-            #calculate adaptive learning rate
+
+            # calculate adaptive learning rate
             lrate = self.lrateRO/(self.regRO + sum(np.power(self.h, 2)))
 
-            #calculate error
+            # calculate error
             error = self.inp - self.out
-        
-            #update weights
-            self.W = self.W + lrate[0,0] * np.multiply(error, self.h.T)
 
-            #decay function for positive weights
+            # update weights
+            self.W = self.W + lrate[0, 0] * np.multiply(error, self.h.T)
+
+            # decay function for positive weights
             if self.decayP > 0:
                 idx = where(self.W > 0)
                 self.W[idx] -= self.decayP * self.W[idx]
-            
-            
-            #decay function for negative weights
+
+            # decay function for negative weights
             if self.decayN == 1:
-                #pure NN weights!
+                # pure NN weights!
                 self.W = np.maximum(self.W, 0)
-            else: 
-            	if self.decayN > 0:
-            		idx = where(self.W < 0)
-            		self.W[idx] -= self.decayN * self.W[idx]
-            
-            
-            #intrinsic plasticity
-            hones = ones((self.hidDim,1))
-            tmp = self.lrateIP * (hones - (2.0 + 1.0/self.meanIP) * self.h + np.power(self.h, 2)/self.meanIP)
+            else:
+                if self.decayN > 0:
+                    idx = where(self.W < 0)
+                    self.W[idx] -= self.decayN * self.W[idx]
+
+            # intrinsic plasticity
+            hones = ones((self.hidDim, 1))
+            tmp = self.lrateIP * \
+                (hones - (2.0 + 1.0/self.meanIP) *
+                 self.h + np.power(self.h, 2)/self.meanIP)
             self.b = self.b + tmp
-            self.a = self.a + self.lrateIP * hones / self.a + np.multiply(self.g, tmp)
-        
-    
-    
-    ## Update network activation
+            self.a = self.a + self.lrateIP * hones / \
+                self.a + np.multiply(self.g, tmp)
+
+    # Update network activation
     # This helper function computes the new activation pattern of the
     # hidden layer for a given input. Note that self.inp field has to be set in advance.
+
     def update(self):
-        #excite network
+        # excite network
         self.g = self.W.T * self.inp
 
-        #apply activation function
+        # apply activation function
         self.h = 1 / (1 + np.exp(np.multiply(-self.a, self.g) - self.b))
 
-        #read-out
-        self.out = self.W * self.h;
-
+        # read-out
+        self.out = self.W * self.h
--- Python/barsExampleNNSAE.py
+++ Python/barsExampleNNSAE.py
@@ -1,8 +1,8 @@
 # This script trains a NNSAE on the synthetic bars data set identifying
-# the independent causes that explain the images. There are 2*dim causes 
-# in the data, where dim is the height/width of the bars images. 
+# the independent causes that explain the images. There are 2*dim causes
+# in the data, where dim is the height/width of the bars images.
 # Play around with the number of hidden neurons in the NNSAE (netDim parameter below)
-# and explore its effects: Too many neurons result in "unused" basis images (see paper). 
+# and explore its effects: Too many neurons result in "unused" basis images (see paper).
 # Also try out different decay factor settings for alpha and beta below.
 #
 ###########################################################################
@@ -16,9 +16,9 @@
 # of the authors. Please acknowledge the authors in any academic publications
 # that have made use of this code or part of it. Please use this BibTex for
 # reference:
-# 
-#    A. Lemme, R. F. Reinhart and J. J. Steil. 
-#    "Online learning and generalization of parts-based image representations 
+#
+#    A. Lemme, R. F. Reinhart and J. J. Steil.
+#    "Online learning and generalization of parts-based image representations
 #     by Non-Negative Sparse Autoencoders". Submitted to Neural Networks.
 #                              OR
 #    A. Lemme, R. F. Reinhart and J. J. Steil. "Efficient online learning of
@@ -33,85 +33,84 @@ import numpy as np
 # Make all matlib functions accessible at the top level via M.func()
 import numpy.matlib as M
 # Make some matlib functions accessible directly at the top level via, e.g. rand(3,3)
-from numpy.matlib import rand,zeros,ones
+from numpy.matlib import rand, zeros, ones
 from createBarsDataSet import createBarsDataSet
 from plotImagesOnGrid import plotImagesOnGrid
 import NNSAE as nn
 import math
 
 ################## Configuration ##########################
-## data parameters
-numSamples = 10000;  #number of images
-width = 9;          #image width = height
+# data parameters
+numSamples = 10000  # number of images
+width = 9  # image width = height
 
 
-## network parameters
-inpDim = width**2;           #number of input/output neurons
-netDim = 2 * width;         #number of hidden neurons matches latent causes
-#netDim = 2 * width + 2;    #number of hidden neurons exceeds latent causes by two
+# network parameters
+inpDim = width**2  # number of input/output neurons
+netDim = 2 * width  # number of hidden neurons matches latent causes
+# netDim = 2 * width + 2;    #number of hidden neurons exceeds latent causes by two
 
-alpha = 1;          #alpha [0..1] is the decay rate for negative weights (alpha = 1 guarantees non-negative weights)
-beta = 0;           #beta [0..1] is the decay rate for positive weights
+# alpha [0..1] is the decay rate for negative weights (alpha = 1 guarantees non-negative weights)
+alpha = 1
+beta = 0           # beta [0..1] is the decay rate for positive weights
 
-#uncomment the following two lines for a symmetric decay function:
-#alpha = 1e-6;          
+# uncomment the following two lines for a symmetric decay function:
+#alpha = 1e-6;
 #beta = 1e-6;
 
-numEpochs = 10;     #number of sweeps through data for learning
-lrateRO = 0.01;     #learning rate for synaptic plasticity of the read-out layer
-lrateIP = 0.001;    #learning rate for intrinsic plasticity
+numEpochs = 10     # number of sweeps through data for learning
+lrateRO = 0.01     # learning rate for synaptic plasticity of the read-out layer
+lrateIP = 0.001    # learning rate for intrinsic plasticity
 
 
 ################## Execution #############################
-## data creation
-X,xTest = createBarsDataSet(width, numSamples, 1, 1);
-#rescale data for better numeric performance
+# data creation
+X, xTest = createBarsDataSet(width, numSamples, 1, 1)
+# rescale data for better numeric performance
 X = 0.25 * X
 
-## network creation
-net = nn.NNSAE(inpDim, netDim	);
-net.lrateRO = lrateRO;
-net.lrateIP = lrateIP;
-net.decayN = alpha;
-net.decayP = beta;
+# network creation
+net = nn.NNSAE(inpDim, netDim)
+net.lrateRO = lrateRO
+net.lrateIP = lrateIP
+net.decayN = alpha
+net.decayP = beta
 
 
-## training
-for e in range(1,numEpochs):
-    print('epoch %d/ %d'%(e,numEpochs))
+# training
+for e in range(1, numEpochs):
+    print(f'epoch {e}\{numEpochs}')
     net.train(X)
 
 
-
 ################## Evaluation ###########################
-## evaluation of basis images
-threshold = 0.1;    #parameter for analysis of weights
+# evaluation of basis images
+threshold = 0.1  # parameter for analysis of weights
 
-#sort basis images for visualization
-cnt = 0;
-unused =[];
+# sort basis images for visualization
+cnt = 0
+unused = []
 w = net.W.T
 v = zeros((w.shape))
-for i in range (1,netDim):
-    if w[i,:].max() > threshold: #this basis image is "used"
+for i in range(1, netDim):
+    if w[i, :].max() > threshold:  # this basis image is "used"
         cnt = cnt + 1
-        v[cnt,:] = w[i,:]
+        v[cnt, :] = w[i, :]
     else:
         unused.append(i)
-    
 
-for i in range(1,len(unused)):
-    v[cnt+i,:] = w[unused[i],:]
 
-print 'used neurons = %d/%d'%(cnt,netDim)
+for i in range(1, len(unused)):
+    v[cnt+i, :] = w[unused[i], :]
+
+print(f'used neurons = {cnt}\{netDim}')
 
 
 ################## Plotting ############################
-## plotting
-numCols = 5;
+# plotting
+numCols = 5
 if netDim >= 50:
     numCols = 10
-    
-plotImagesOnGrid(v, int(math.ceil(netDim/numCols)), numCols, width, width,range(0,11),'./fig/NNSAE-bars-%d-basis.png'%(netDim))
 
-
+plotImagesOnGrid(v, int(math.ceil(netDim/numCols)), numCols, width,
+                 width, range(0, 11), './fig/NNSAE-bars-%d-basis.png' % (netDim))
