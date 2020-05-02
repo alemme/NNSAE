8fceb8861f42546ffd09eeb02cd1dfc226af89c8
Andre Lemme <alemme01@t-online.de>
2020-05-02 08:29:56 +0200

    added test loop with evaluation plot

--- Pytorch/mnistExampleNNSAE.py
+++ Pytorch/mnistExampleNNSAE.py
@@ -29,16 +29,18 @@
 #                           alemme_at_cor-lab.uni-bielefeld.de
 
 # Make all numpy available via shorter 'num' prefix
+import os
 import math
 import NNSAE as nn
 import torch
 from torchvision import datasets, transforms
-from plotImagesOnGrid import plotImagesOnGrid
+from plotImagesOnGrid import plotImagesOnGrid, plotPaperPlot
 from numpy.matlib import zeros
 import numpy as np
 np.random.seed(1234)
 use_cuda = torch.cuda.is_available()
 torch.manual_seed(1234)
+fileName = 'mnistExampleNNSAE.pt'
 device = torch.device("cuda" if use_cuda else "cpu")
 kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 batch_size = 64
@@ -70,37 +72,40 @@ bpdc = nn.BackpropagationDecoralation(
 loss_fkt = torch.nn.modules.MSELoss(reduction='mean')
 net.lrateRO = lrateRO
 net.lrateIP = lrateIP
-numSamples = len(train_loader)
-# training
-for e in range(1, numEpochs+1):
-    gl_loss = 0
-    numSamples = 0
+numBatches = len(train_loader)
+if not os.path.isfile(fileName):
+    # training
+    for e in range(1, numEpochs+1):
+        gl_loss = 0
+        numSamples = 0
 
-    for i, data in enumerate(train_loader):
-        # bpdc.zero_grad()
-        with torch.no_grad():
-            inp = data[0].view(data[0].shape[0], width**2)
-            numSamples += data[0].shape[0]
-            # forward path
-            out = net(inp.t()).t()
-            # calculate loss
-            loss = loss_fkt(inp, out)
-            # loss.backward()
+        for i, data in enumerate(train_loader):
+            # bpdc.zero_grad()
+            with torch.no_grad():
+                inp = data[0].view(data[0].shape[0], width**2)
+                numSamples += data[0].shape[0]
+                # forward path
+                out = net(inp.t()).t()
+                # calculate loss
+                loss = loss_fkt(inp, out)
+                # loss.backward()
 
-            # bpdc.step()
-            net.bpdc((inp-out).t())
+                # bpdc.step()
+                net.bpdc((inp-out).t())
 
-            # non negative constraint
-            net.weights.data[net.weights < 0] = 0
-            # intrinsic plasticity
-            net.ip()
+                # non negative constraint
+                net.weights.data[net.weights < 0] = 0
+                # intrinsic plasticity
+                net.ip()
 
-            # log loss
-            gl_loss += loss.item()
+                # log loss
+                gl_loss += loss.item()
 
-    # print(f'epoch ({e}\{numEpochs}) loss {gl_loss/numSamples}')
-    print('epoch ({}\{}) loss {}'.format(e, numEpochs, gl_loss/numSamples))
-
+        # print(f'epoch ({e}\{numEpochs}) loss {gl_loss/numSamples}')
+        print('epoch ({}\{}) loss {}'.format(e, numEpochs, gl_loss/numSamples))
+    net.save_state_dict(fileName)
+else:
+    net.load_state_dict(torch.load(fileName))
 ################## Evaluation ###########################
 # evaluation of basis images
 threshold = 0.1  # parameter for analysis of weights
@@ -132,3 +137,32 @@ if netDim >= 50:
 
 plotImagesOnGrid(v, int(math.ceil(netDim/numCols)), numCols, width,
                  width, range(netDim), './fig/NNSAE-bars-%d-basis.png' % (netDim))
+
+gl_loss = 0
+numSamples = 0
+net.eval()
+orig = []
+approx = []
+activations = []
+with torch.no_grad():
+    num = 0
+    for i, data in enumerate(test_loader):
+        inp = data[0].view(data[0].shape[0], width**2)
+        label = data[1]
+
+        numSamples += data[0].shape[0]
+        # forward path
+        out = net(inp.t()).t()
+        # calculate loss
+        loss = loss_fkt(inp, out)
+        label_list = label.tolist()
+        while num in label_list:
+            num_index = label_list.index(num)
+            orig.append(inp[num_index, :])
+            approx.append(out[num_index, :])
+            activations.append(net.h.t()[num_index, :])
+            num += 1
+
+        gl_loss += loss.item()
+    print('Evaluation loss MSE: {}'.format(gl_loss/numSamples))
+    plotPaperPlot(orig, approx, activations, width, width)
